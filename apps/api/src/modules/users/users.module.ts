50e9c5174e0039184e60419924cf00331e1187e5
tomastoromoreno-ctrl <autorestchile@gmail.com>
2026-06-26 11:38:19 -0400

    fix(api): Import PermissionsModule in UsersModule to fix DI error

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserInvitationsService } from './user-invitations.service';
+import { PermissionsModule } from '../permissions/permissions.module';
 
 @Module({
+  imports: [PermissionsModule],
   controllers: [UsersController],
   providers: [UsersService, UserInvitationsService],
   exports: [UsersService, UserInvitationsService],
